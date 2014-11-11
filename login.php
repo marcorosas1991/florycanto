445f346c6708eff4061da5139f7193ba2636ca37
Emilio Cornejo <emiliocornejo@MacBook-Pro-de-Emilio.local>
2014-11-11 12:53:55 -0600

    emilio mods

--- login.php
+++ login.php
@@ -1,12 +1,18 @@
 <style>
 
+@import url(http://fonts.googleapis.com/css?family=Courgette);
+
+*{
+    font-family: 'Courgette';
+}
+    
 .boton
 {
     margin-top: 10px;
     font-size: 1em !important;
     font-family: 'Ubuntu Mono' !important;
     border-radius: .3em;
-
+    
     letter-spacing:2px;
     width: 25%;
     height: 30px;
@@ -17,27 +23,10 @@
     background-image: linear-gradient(45deg, rgba(0, 0, 0, 0.05) 25%, transparent 25%, transparent),
                       linear-gradient(-45deg, rgba(0, 0, 0, 0.05) 25%, transparent 25%, transparent),
                       linear-gradient(45deg, transparent 75%, rgba(0, 0, 0, 0.05) 75%),
-<<<<<<< HEAD
-                      linear-gradient(-45deg, transparent 75%, rgba(0, 0, 0, 0.05) 75%);
-}
-
-
-.header{
-    font-color:#7DB739;
-    font-family: Monotype-cursiva;
-}
-
-=======
                       linear-gradient(-45deg, transparent 75%, rgba(0, 0, 0, 0.05) 75%); 
 }
 
     
-.header{
-    font-color:#7DB739;
-    font-family: Monotype-cursiva;
-}  
-    
->>>>>>> alan
 </style>
 
 <html>
@@ -47,12 +36,12 @@
 
   <body>
     <center><img src="http://i98.photobucket.com/albums/l273/Pompovaz/florycanto_zps017d7eb8.png"/>
-    <p class="header" style="color: #E12A83; font-family: Monotype corsiva; font-size: 30px;"> Ingreso al Sistema </p>
+    <p class="header" style="color: #E12A83; font-family: Courgette; font-size: 30px;"> Ingreso al Sistema </p>
     <form action="validaUsuario.php" method="post">
-      <span style="font-family: Monotype corsiva;">Usuario: <input type="text" name="user"> </span>
+      <span style="font-family: Courgette;">Usuario: <input type="text" name="user"> </span>
       <br>
       <br>
-      <span style="font-family: Monotype corsiva; font-color: #444444;">Contrase&ntilde;a: <input type="password" name="password"> </span>
+      <span style="font-family: Courgette; font-color: #444444;">Contrase&ntilde;a: <input type="password" name="password"> </span>
       <br>
       <br>
       <input type="submit" value="Ingresar" class="boton">
